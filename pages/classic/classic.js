2c7209b4fc5d707a93cee4710e28398a5decc4f7
xuyu <xypisces@163.com>
2019-01-04 15:52:05 +0800

    http函数封装

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,4 +1,6 @@
 // pages/classic/classic.js
+import { HTTP } from '../../utils/http.js'
+let http = new HTTP()
 Page({
 
   /**
@@ -12,7 +14,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    http.request({
+      url: 'classic/latest',
+      success:(res) => {
+        console.log(res)
+      }
+    })
   },
 
   /**
---
+++ utils/http.js
@@ -0,0 +1,48 @@
+ import {
+   config
+ } from '../config.js'
+
+ const tips = {
+   1: '抱歉出现了一个错误'
+ }
+ class HTTP {
+   request(params) {
+     if (!params.method) {
+       params.method = 'GET'
+     }
+     wx.request({
+       url: config.api_base_url + params.url,
+       data: params.data,
+       header: {
+         'content-type': 'application/json',
+         'appkey': config.appkey,
+       },
+       method: params.method,
+       dataType: 'json',
+       success: (res) => {
+         let code = res.statusCode.toString()
+         if (code.startsWith('2')) {
+           params.success(res)
+         } else {
+           this._show_error()
+         }
+       },
+       fail: (err) => {
+         console.error(err)
+         this._show_error()
+       },
+       complete: (res) => {},
+     })
+   }
+   _show_error(code = 1) {
+     wx.showToast({
+       title: tips[code],
+       icon: 'none',
+       duration: 2000,
+     })
+   }
+ }
+
+ export {
+   HTTP
+ }
